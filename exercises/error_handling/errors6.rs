9a23bc48d45e09de0bed7ab09a698c23805d805b
Your Name <you@example.com>
2025-09-25 20:59:08 +0800

    27

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -1,19 +1,6 @@
-// errors6.rs
-//
-// Using catch-all error types like `Box<dyn error::Error>` isn't recommended
-// for library code, where callers might want to make decisions based on the
-// error content, instead of printing it out or propagating it further. Here, we
-// define a custom error type to make it possible for callers to decide what to
-// do next when our function returns an error.
-//
-// Execute `rustlings hint errors6` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
-// This is a custom error type that we will be using in `parse_pos_nonzero()`.
+// 自定义错误类型，统一处理解析和创建过程中的错误
 #[derive(PartialEq, Debug)]
 enum ParsePosNonzeroError {
     Creation(CreationError),
@@ -21,22 +8,25 @@ enum ParsePosNonzeroError {
 }
 
 impl ParsePosNonzeroError {
+    // 从CreationError转换
     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
-    // TODO: add another error conversion function here.
-    // fn from_parseint...
+    
+    // 从ParseIntError转换
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
-    // TODO: change this to return an appropriate error instead of panicking
-    // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    // 使用map_err将ParseIntError转换为自定义错误类型
+    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parseint)?;
+    // 将CreationError转换为自定义错误类型
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
 
-// Don't change anything below this line.
-
+// 以下代码保持不变
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
 
@@ -62,7 +52,6 @@ mod test {
 
     #[test]
     fn test_parse_error() {
-        // We can't construct a ParseIntError, so we have to pattern match.
         assert!(matches!(
             parse_pos_nonzero("not a number"),
             Err(ParsePosNonzeroError::ParseInt(_))
@@ -87,8 +76,7 @@ mod test {
 
     #[test]
     fn test_positive() {
-        let x = PositiveNonzeroInteger::new(42);
-        assert!(x.is_ok());
-        assert_eq!(parse_pos_nonzero("42"), Ok(x.unwrap()));
+        let x = PositiveNonzeroInteger::new(42).unwrap();
+        assert_eq!(parse_pos_nonzero("42"), Ok(x));
     }
 }

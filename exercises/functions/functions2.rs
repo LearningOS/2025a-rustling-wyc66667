0f120a9d5fdf3cf9e87f28cd05d149e5f2a691e9
Your Name <you@example.com>
2025-09-25 21:06:34 +0800

    29

--- exercises/functions/functions2.rs
+++ exercises/functions/functions2.rs
@@ -1,14 +1,9 @@
-// functions2.rs
-//
-// Execute `rustlings hint functions2` or use the `hint` watch subcommand for a
-// hint.
-
-
 fn main() {
     call_me(3);
 }
 
-fn call_me(num: i32) {
+// 将参数类型从i32改为usize，匹配Rust范围语法要求
+fn call_me(num: usize) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
